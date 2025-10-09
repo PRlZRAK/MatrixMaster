f6e9f52cead7842f81b1b275e7fd155d2dc94fe5
Alexey Rodygin <alesharodygin@gmail.com>
2025-10-09 12:45:29 +0200

    update2

--- JavaScript/Assignment1BringEverythingTogether/java.js
+++ JavaScript/Assignment1BringEverythingTogether/java.js
@@ -6,6 +6,15 @@ function tierListSubmit() {
   var cell = row.insertCell();
   console.log(fill, row);
   cell.innerHTML = `<h3>${fill}</h3>`;
+  var trlength = document.getElementById("yourTable").rows.length;
+  var pId = ["thead", "slengh", "alengh", "blengh", "clengh", "dlengh"];
+  for (let i = 1; i < trlength; i++) {
+    document.getElementById(
+      pId[i]
+    ).innerHTML = `Number of elements ih row ${i} is ${
+      document.getElementById("yourTable").rows[i].cells.length - 1
+    }`;
+  }
 }
 
 var theFurthestText = document.getElementById("veryFarDiv");
