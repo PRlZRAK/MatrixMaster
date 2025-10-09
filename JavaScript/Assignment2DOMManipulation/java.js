5106102b00d75b58e17b50060de5357a8e14e151
Alexey Rodygin <alesharodygin@gmail.com>
2025-10-09 16:25:23 +0200

    finished

--- JavaScript/Assignment2DOMManipulation/java.js
+++ JavaScript/Assignment2DOMManipulation/java.js
@@ -3,6 +3,11 @@ document
   .addEventListener("click", function (event) {
     event.preventDefault();
     document.getElementById("my-form").style.background = "#ff0000ff";
-    document.getElementsByClassName("bg-dark")[0].style.color = "#ffffffff";
-    document.getElementsByClassName("bg-dark").style.background = "#3f3f3fff";
+    document.querySelector("body").classList.add("bg-dark");
+    let body = document.getElementsByClassName("bg-dark")[0];
+    body.style.background = "#3f3f3fff";
+    body.style.color = "#ffffffff";
+    document.getElementsByClassName("items")[0].lastElementChild.innerHTML =
+      "<h1>Hello</h1>";
+    this.style.background = "#00ff00ff";
   });
