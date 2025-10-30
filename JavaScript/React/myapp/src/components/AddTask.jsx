76cbed39a832d62896c3c5f223e14a72ffdb6c32
Alexey Rodygin <alesharodygin@gmail.com>
2025-10-30 16:10:13 +0100

    done

--- JavaScript/React/myapp/src/components/AddTask.jsx
+++ JavaScript/React/myapp/src/components/AddTask.jsx
@@ -1,32 +1,67 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class AddTask extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            tasks: [],
-            task:'',
-            description:''
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [],
+      task: "",
+      description: "",
+    };
+  }
   render() {
     return (
-        <div>
-            <h1>New Task:</h1>
-        <input value={this.state.task} onChange={(e)=>{this.setState({task:e.target.value})}} type="text" placeholder='Your task...' />
-        <input value={this.state.description} onChange={(e)=>{this.setState({description:e.target.value})}} type="text"placeholder='Task description...' />
-        <button onClick={() =>{this.setState({
-             tasks: [...this.state.tasks, {task: this.state.task, description: this.state.description}]
-           })}}>Add</button>
+      <div className="mainDiv">
+        <h1>New Task:</h1>
+        <input
+          value={this.state.task}
+          onChange={(e) => {
+            this.setState({ task: e.target.value });
+          }}
+          type="text"
+          placeholder="Your task..."
+        />
+        <textarea
+          value={this.state.description}
+          onChange={(e) => {
+            this.setState({ description: e.target.value });
+          }}
+          placeholder="Task description..."
+        />
+        <button
+          onClick={() => {
+            if (this.state.task == "") {
+              alert("Task cannot be empty!");
+              return;
+            }
+            this.setState({
+              tasks: [
+                ...this.state.tasks,
+                { task: this.state.task, description: this.state.description },
+              ],
+            });
+          }}
+        >
+          Add
+        </button>
         <h1>My todolist:</h1>
-        <ul id='todo'>
-{
-            this.state.tasks.map((item) => (
-                <li>{item.task} - {item.description} <button onClick={()}>Done</button></li>
-            ))
-}
+        <ul id="todo">
+          {this.state.tasks.map((p) => (
+            <li>
+              <p className="taskP">{p.task}:</p> {p.description}
+              <button
+                onClick={() => {
+                  this.setState({
+                    tasks: this.state.tasks.filter((e) => e !== p),
+                  });
+                }}
+              >
+                Done
+              </button>
+            </li>
+          ))}
         </ul>
-        </div>
-
-    );}}
-    
+      </div>
+    );
+  }
+}
