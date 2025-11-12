1dedd6105b70eeae0ce8c20b6f59e3100d659481
Alexey Rodygin <82274870+PRlZRAK@users.noreply.github.com>
2025-11-12 02:02:54 +0100

    assignment2

--- JavaScript/React/Assignment2QuotesGenerator/quotegenerator/src/App.js
+++ JavaScript/React/Assignment2QuotesGenerator/quotegenerator/src/App.js
@@ -1,1 +1,40 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [quotes, setQ] = useState([]);
+
+  function getRandomQuote() {
+    axios
+      .get("https://dummyjson.com/quotes/random")
+      .then((result) => {
+        setQ(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  useEffect(() => {
+    getRandomQuote();
+  }, []);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Quotes Generator</h1>
+      </header>
+      <main>
+        <p className="quote">{quotes.data?.quote}</p>
+        <p className="author"> "{quotes.data?.author}"</p>
+      </main>
+      <footer>
+        <button onClick={getRandomQuote}>New Quote</button>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
 
