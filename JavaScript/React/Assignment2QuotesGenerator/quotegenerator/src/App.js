e9733de41f3098017d2408fe55bfba6d4600a73b
Alexey Rodygin <alesharodygin@gmail.com>
2025-11-12 02:08:07 +0100

    .

--- JavaScript/React/Assignment2QuotesGenerator/quotegenerator/src/App.js
+++ JavaScript/React/Assignment2QuotesGenerator/quotegenerator/src/App.js
@@ -26,7 +26,7 @@ function App() {
         <h1>Quotes Generator</h1>
       </header>
       <main>
-        <p className="quote">{quotes.data?.quote}</p>
+        <p>{quotes.data?.quote}</p>
         <p className="author"> "{quotes.data?.author}"</p>
       </main>
       <footer>
@@ -37,43 +37,3 @@ function App() {
 }
 
 export default App;
-
-// import React, { useState, useEffect } from "react";
-// import "./App.css";
-// import axios from "axios";
-
-// function App() {
-//   const [quote, setQuote] = useState("");
-//   const [author, setAuthor] = useState("");
-
-//   function getQuote() {
-//     axios
-//       .get("https://dummyjson.com/quotes/random")
-//       .then((result) => {
-//         setQuote(result.data.quote);
-//         setAuthor(result.data.author || "Unknown");
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-//   }
-
-//   useEffect(() => {
-//     getQuote();
-//   }, []);
-
-//   return (
-//     <div className="container">
-//       <h1 className="title">Quote Generator</h1>
-//       <div className="quote-box">
-//         <p className="quote-text">{quote}</p>
-//         <p className="quote-author"> "{author}"</p>
-//       </div>
-//       <button className="new-quote-btn" onClick={getQuote}>
-//         New Quote
-//       </button>
-//     </div>
-//   );
-// }
-
-// export default App;
